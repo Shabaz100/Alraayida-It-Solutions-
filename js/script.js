4b3858aa0dcbd77f0c9592422f07e652992709e8
Shabazmahamood <152277607+Shabaz100@users.noreply.github.com>
2024-05-23 00:32:03 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,109 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++) {
+  elemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+/**
+ * toggle navbar & overlay when click any navbar-link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+
+
+
+
+/**
+ * header & go-top-btn active
+ * when window scroll down to 400px
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 400) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
+
+// LEADERSHIP SLIDER START =================================================================
+$('.leadership_slider').slick({
+  slidesToShow:4,
+  autoplay:true,
+  speed: 1000,
+  prevArrow: '<i class="fa-solid fa-chevron-left leadership_slider_arrows"></i>',
+  nextArrow: '<i class="fa-solid fa-chevron-right leadership_slider_arrows"></i>',
+  responsive: [
+      {
+          breakpoint: 1200,
+          settings: {
+              slidesToShow: 3,
+          },
+      },
+      {
+          breakpoint: 992,
+          settings: {
+              slidesToShow: 2,
+          },
+      },
+      {
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 2,
+          },
+      },
+      {
+          breakpoint: 576,
+          settings: {
+              slidesToShow: 1,
+          },
+      }
+  ]
+});
+// LEADERSHIP SLIDER END =================================================================
+
+ // ABOUT ANIMATE SLIDER START =================================================================
+ $('.animate_silder').slick({
+  slidesToShow:1,
+  arrows: false,
+  autoplay: true,
+  fade: true,
+  speed:1000,
+  dots: true,
+  appendDots: $(".animation_slider_dots"),
+  dotsClass: "animate_slider_dots"
+});
+// ABOUT ANIMATE SLIDER END =================================================================
+
+
+
+
